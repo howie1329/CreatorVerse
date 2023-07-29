6059d21ac8fc6a59eeb83a22a16ac932abe23e02
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-07-28 23:57:22 -0400

    browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
 /* eslint-disable react/jsx-no-target-blank */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import AddCreator from './pages/AddCreator'
+import EditCreator from './pages/EditCreator'
+import ShowCreators from './pages/ShowCreators'
+import ViewCreator from './pages/ViewCreator'
+import { useEffect, useState } from 'react'
+import { supabaseClients } from './client'
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface creatortemp {
+    id: number
+    name: string
+    url: string
+    description: string
+    imageURL:string
+  }
+
+  interface all {
+    stuff: [creatortemp]
+  }
+
+  const [creators, setCreators] = useState<all | undefined>(undefined)
+
+  const fetchData = async () => {
+    let {data: creators, error} = await supabaseClients.from("creators").select("*")
+    return creators
+  }
+  useEffect(() => {
+    const data = fetchData()
+    data.then(setCreators)
+    setCreators(data)
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<ShowCreators />} />
+      <Route path = "/addCreator" element = {<AddCreator />} />
+      <Route path = "/editCreator" element = {<EditCreator />} />
+      <Route path = "/viewCreator" element = {<ViewCreator />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/pages/ShowCreators.tsx
+++ src/pages/ShowCreators.tsx
@@ -1,6 +1,9 @@
-
+import { useEffect } from "react"
 
 export default function ShowCreators() {
+  useEffect(() => {
+
+  })
   return (
     <div>ShowCreators</div>
   )
